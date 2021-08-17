17b29394934e8924f891dbbd743e661dd7210dd8
zuzikx3 <z.plauszta@gmail.com>
2021-08-17 12:40:44 +0200

    refactor reading file method

--- src/test/java/pl/plauszta/KnapsackTest.java
+++ src/test/java/pl/plauszta/KnapsackTest.java
@@ -15,23 +15,7 @@ class KnapsackTest {
     @Test
     @DisplayName("Testing algorithm for the in.txt file")
     void checkResultOfAlgorithm() {
-        Knapsack knapsack = null;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filesPath + "in.txt"))) {
-            String line = bufferedReader.readLine();
-            String[] splitLine = line.split(" ");
-            knapsack = new Knapsack(Integer.parseInt(splitLine[0]), Integer.parseInt(splitLine[1]));
-
-            while ((line = bufferedReader.readLine()) != null) {
-                splitLine = line.split(" ");
-                knapsack.add(new Thing(
-                        Integer.parseInt(splitLine[0]),
-                        Integer.parseInt(splitLine[1]),
-                        Integer.parseInt(splitLine[2]))
-                );
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        Knapsack knapsack = readKnapsackFromFile("in.txt");
         if (knapsack == null) {
             return;
         }
@@ -43,23 +27,7 @@ class KnapsackTest {
     @Test
     @DisplayName("Testing algorithm for the in1.txt file")
     void checkResultOfAlgorithm2() {
-        Knapsack knapsack = null;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filesPath + "in1.txt"))) {
-            String line = bufferedReader.readLine();
-            String[] splitLine = line.split(" ");
-            knapsack = new Knapsack(Integer.parseInt(splitLine[0]), Integer.parseInt(splitLine[1]));
-
-            while ((line = bufferedReader.readLine()) != null) {
-                splitLine = line.split(" ");
-                knapsack.add(new Thing(
-                        Integer.parseInt(splitLine[0]),
-                        Integer.parseInt(splitLine[1]),
-                        Integer.parseInt(splitLine[2]))
-                );
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        Knapsack knapsack = readKnapsackFromFile("in1.txt");
         if (knapsack == null) {
             return;
         }
@@ -71,8 +39,18 @@ class KnapsackTest {
     @Test
     @DisplayName("Testing algorithm for the in2.txt file")
     void checkResultOfAlgorithm3() {
+        Knapsack knapsack = readKnapsackFromFile("in2.txt");
+        if (knapsack == null) {
+            return;
+        }
+        Result expectedResult = new Result(25, Arrays.asList(1, 3, 4, 5, 8, 9));
+        Result result = knapsack.getResult();
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    private Knapsack readKnapsackFromFile(String file) {
         Knapsack knapsack = null;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filesPath + "in2.txt"))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filesPath + file))) {
             String line = bufferedReader.readLine();
             String[] splitLine = line.split(" ");
             knapsack = new Knapsack(Integer.parseInt(splitLine[0]), Integer.parseInt(splitLine[1]));
@@ -88,11 +66,6 @@ class KnapsackTest {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        if (knapsack == null) {
-            return;
-        }
-        Result expectedResult = new Result(25, Arrays.asList(1, 3, 4, 5, 8, 9));
-        Result result = knapsack.getResult();
-        Assertions.assertEquals(expectedResult, result);
+        return knapsack;
     }
 }
