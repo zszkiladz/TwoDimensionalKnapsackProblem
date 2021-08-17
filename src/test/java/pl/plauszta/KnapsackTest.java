cac4252355e4683bee7594cea0b32dfcb3b09be5
zuzikx3 <z.plauszta@gmail.com>
2021-08-17 14:01:39 +0200

    refactor

--- src/test/java/pl/plauszta/KnapsackTest.java
+++ src/test/java/pl/plauszta/KnapsackTest.java
@@ -18,11 +18,8 @@ class KnapsackTest {
     @DisplayName("Testing algorithm for the in.txt file")
     void checkResultOfAlgorithm() {
         Knapsack knapsack = readKnapsackFromFile("in.txt");
-        if (knapsack == null) {
-            return;
-        }
-        Result expectedResult = new Result(5, Arrays.asList(1, 3));
         Result result = knapsack.getResult();
+        Result expectedResult = new Result(5, Arrays.asList(1, 3));
         Assertions.assertEquals(expectedResult, result);
     }
 
@@ -30,11 +27,8 @@ class KnapsackTest {
     @DisplayName("Testing algorithm for the in1.txt file")
     void checkResultOfAlgorithm2() {
         Knapsack knapsack = readKnapsackFromFile("in1.txt");
-        if (knapsack == null) {
-            return;
-        }
-        Result expectedResult = new Result(11, Arrays.asList(1, 2, 5, 6));
         Result result = knapsack.getResult();
+        Result expectedResult = new Result(11, Arrays.asList(1, 2, 5, 6));
         Assertions.assertEquals(expectedResult, result);
     }
 
@@ -42,11 +36,8 @@ class KnapsackTest {
     @DisplayName("Testing algorithm for the in2.txt file")
     void checkResultOfAlgorithm3() {
         Knapsack knapsack = readKnapsackFromFile("in2.txt");
-        if (knapsack == null) {
-            return;
-        }
-        Result expectedResult = new Result(25, Arrays.asList(1, 3, 4, 5, 8, 9));
         Result result = knapsack.getResult();
+        Result expectedResult = new Result(25, Arrays.asList(1, 3, 4, 5, 8, 9));
         Assertions.assertEquals(expectedResult, result);
     }
 
@@ -55,7 +46,6 @@ class KnapsackTest {
         if (fileUrl == null) {
             throw new RuntimeException("Cannot find file '" + filepath + "'");
         }
-
         try {
             List<String> fileContent = Files.readAllLines(Paths.get(fileUrl.toURI()));
             String[] knapsackConstraintsLine = fileContent.get(0).split(" ");
@@ -69,7 +59,6 @@ class KnapsackTest {
                         Integer.parseInt(thingLine[2]))
                 );
             }
-
             return knapsack;
         } catch (IOException | URISyntaxException ex) {
             throw new RuntimeException("Cannot read knapsack", ex);
